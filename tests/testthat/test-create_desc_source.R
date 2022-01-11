ccde704384f0ad24353e408438df06b10eb5bff8
kosin <statismike@gmail.com>
2022-01-11 17:53:09 +0100

    secrets in vignettes and tests

--- tests/testthat/test-create_desc_source.R
+++ tests/testthat/test-create_desc_source.R
@@ -1,4 +1,4 @@
-test_that("Create description source", {
+test_that("Create description source to df works", {
   
   descriptions_source <- create_desc_source(
     method = "df"
@@ -13,4 +13,35 @@ test_that("Create description source", {
     c("type", "align", "html", "order", "inputId", "content")
   )
   
+})
+
+test_that("Create description source to gsheets works", {
+  
+  suppressMessages({
+    googlesheets4::gs4_auth(email = Sys.getenv("G_SERVICE_MAIL"),
+                            path = Sys.getenv("G_SERVICE_ACCOUNT"))
+    googledrive::drive_auth(email = Sys.getenv("G_SERVICE_MAIL"),
+                            path = Sys.getenv("G_SERVICE_ACCOUNT"))
+  })
+
+  
+  descriptions_id <- create_desc_source(
+    method = "gsheet"
+  )
+  
+  descriptions_source <- googlesheets4::read_sheet(
+    ss = descriptions_id,
+    sheet = "Descriptions"
+  )
+  
+  expect_equal(nrow(descriptions_source), 1)
+  
+  expect_equal(
+    names(descriptions_source),
+    c("type", "align", "html", "order", "inputId", "content")
+  )
+  
+  suppressMessages({
+    googledrive::drive_trash(descriptions_id)
+  })
 })
